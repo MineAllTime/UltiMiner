3b33b4964f8342f98a6cfbc3223fbd7f62a766f7
MineAllTime <maksimnazarov2508@gmail.com>
2025-05-20 03:19:10 +0500

    Create UltiMinerMod.java

---
+++ src/main/resources/src/main/java/net/minealltime/ultiminer/UltiMinerMod.java
@@ -0,0 +1,10 @@
+package net.minealltime.ultiminer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UltiMinerMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("UltiMiner loaded!");
+    }
+}
